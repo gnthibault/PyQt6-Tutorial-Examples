e7c20da7b7965eb8502010c2126b2cb51d8d3d65
gnthibault <gnthibault@gmail.com>
2021-09-15 15:30:39 +0200

    Trying out Pyqt5, this is not working at all

--- pyqt3d-webgl/run.sh
+++ pyqt3d-webgl/run.sh
@@ -3,14 +3,22 @@
 
 # You will need (in order)
 
+# Download vulkan on https://vulkan.lunarg.com/sdk/home
+# Build with Vulkan (Vulkan itself just need to be untared into /opt/Vulkan for instance):
+# sudo apt install libglm-dev cmake libxcb-dri3-0 libxcb-present0 libpciaccess0 \
+# libpng-dev libxcb-keysyms1-dev libxcb-dri3-dev libx11-dev g++ gcc g++-multilib \
+# libmirclient-dev libwayland-dev libxrandr-dev libxcb-randr0-dev libxcb-ewmh-dev \
+# git python3 bison libx11-xcb-dev liblz4-dev libzstd-dev python3-distutils qt5-default
+# Minimum CMake 3.10.2 version is required.
+# source /path_to_vulkan/1.x.yy.z/setup-env.sh
+
+
+# For QT 5.15.1 - QtWebGLPlugin 6.2.0
+
+
 # git@github.com:qt/qtbase.git
-# build with cmake
+# build with ./configure, then cmake
 
-# git@github.com:qt/qtshadertools.git
-# Build with export Qt6CoreTools_DIR=/usr/local/Qt-6.2.0/lib/cmake/Qt6CoreTools/; export Qt6_DIR=/usr/local/Qt-6.2.0/; cmake ../../qtshadertools
-
-# git@github.com:qt/qtdeclarative.git
-# build with export Qt6CoreTools_DIR=/usr/local/Qt-6.2.0/lib/cmake/Qt6CoreTools/; export Qt6_DIR=/usr/local/Qt-6.2.0/; cmake ../../qtdeclarative/
 
 # git@github.com:qt/qt3d.git
 # build with export Qt6CoreTools_DIR=/usr/local/Qt-6.2.0/lib/cmake/Qt6CoreTools/; export Qt6_DIR=/usr/local/Qt-6.2.0/; cmake ../../qt3d
@@ -23,8 +31,14 @@
 # https://github.com/qt/qtwebglplugin
 # build with /usr/local/Qt-6.2.0/bin/qmake ../../qtwebglplugin/
 
+# For QT 6.2.0 - QtWebGLPlugin ???? you can add the followings (not even sure if useful...)
 
-# preferably all at tag >=6.2.0
+# git@github.com:qt/qtshadertools.git
+# Build with export Qt6CoreTools_DIR=/usr/local/Qt-6.2.0/lib/cmake/Qt6CoreTools/; export Qt6_DIR=/usr/local/Qt-6.2.0/; cmake ../../qtshadertools
+
+# git@github.com:qt/qtdeclarative.git
+# build with export Qt6CoreTools_DIR=/usr/local/Qt-6.2.0/lib/cmake/Qt6CoreTools/; export Qt6_DIR=/usr/local/Qt-6.2.0/; cmake ../../qtdeclarative/
+
 
 # First, make sure you have webgl plugin in /usr/local/Qt-6.2.0/plugins/platforms/
 # otherwise, you'll see something like:
@@ -37,4 +51,4 @@
 export QSG_INFO=1
 export QT_QPA_EGLFS_DEBUG=1
 export QT_LOGGING_RULES=qt.qpa.*=true
-QT_QPA_PLATFORM=webgl:port=8998 python ../pyqt3d/test.py
+QT_QPA_PLATFORM=webgl:port=8998 python ../pyqt3d/testQt5.py
